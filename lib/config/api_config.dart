7b551de380354069e34c7c65abf844800fde4ea5
Jay2004mistry <jaymistry6565@gmail.com>
2026-09-07 09:16:35 +0530

    chatstillneed to see

--- lib/config/api_config.dart
+++ lib/config/api_config.dart
@@ -1,11 +1,16 @@
+import 'package:flutter/foundation.dart';
+
 class ApiConfig {
   static const bool isEmulator = bool.fromEnvironment('IS_EMULATOR', defaultValue: false);
 
   static String get baseUrl {
-    if (isEmulator) {
+    if (kIsWeb) {
+      return "http://localhost:8080";
+    } else if (isEmulator) {
       return "http://10.0.2.2:8080";
     } else {
-      return "http://10.25.30.155:8080";
+      return "http://192.168.0.102:8080";
     }
   }
 }
+
